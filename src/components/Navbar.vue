00cada66db5ed42ec227c93d1337df680d4a7b98
niphitphon123 <112224187+niphitphon123@users.noreply.github.com>
2022-12-01 17:26:08 +0700

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar">
+        <a href="/"> <i class="fas fa-user"></i> <span>Home</span> </a>
+        <a href="/products"> <i class="fas fa-address-book"></i> <span>Products</span> </a>
+        <a href="/order"> <i class="fas fa-address-book"></i> <span>Order</span> </a>
+        <a href="/presenter"> <i class="fas fa-address-book"></i> <span>Presenter</span> </a>
+        
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
